7e44e2da0809223fb1d44038b85c7ff8bb3bd867
TejaChittamuri <111578142+Teja-Chittamuri@users.noreply.github.com>
2024-12-05 19:33:56 +0530

    Update key-vault.tf

--- key-vault.tf
+++ key-vault.tf
@@ -4,9 +4,6 @@ resource "azurerm_key_vault" "key_vault" {
   resource_group_name = var.resource_group_name
   tenant_id           = var.tenant_id
   sku_name            = "standard"
-  soft_delete_retention_days = var.soft_delete_retention_days
-  purge_protection_enabled = var.purge_protection_enabled
-  enabled_for_disk_encryption = var.enabled_for_disk_encryption
   tags                = var.tags
 
   network_acls {
