9bd0b7b393b401dd5a4690364290307d24431133
TejaChittamuri <111578142+Teja-Chittamuri@users.noreply.github.com>
2024-12-05 19:43:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ variable "default_action" {
    type = string
    default = "Deny"
 }
+
+variable "soft_delete_retention_days" {
+   type = number
+   default = 90
+}
+
+variable "purge_protection_enabled" {
+   type = boolean
+   default = true
+}
+
+variable "enabled_for_disk_encryption" {
+   type = boolean
+   default = false
+}
