05b0f60e89386a68567357a6fde0574e6c925bc1
TejaChittamuri <111578142+Teja-Chittamuri@users.noreply.github.com>
2024-12-05 21:57:54 +0530

    Update key-vault.tf

--- key-vault.tf
+++ key-vault.tf
@@ -11,6 +11,6 @@ resource "azurerm_key_vault" "key_vault" {
 
   network_acls {
     bypass                = "AzureServices"
-    default_action        = "Deny"
+    default_action        = var.default_action
   }
 
