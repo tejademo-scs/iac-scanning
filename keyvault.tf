3596de0e7ff515686d541f6c6dbe52b047e02a2d
TejaChittamuri <111578142+Teja-Chittamuri@users.noreply.github.com>
2024-12-12 21:35:04 +0530

    Update keyvault.tf

--- keyvault.tf
+++ keyvault.tf
@@ -1,29 +1,22 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resource-group"
-  location = "West US"
-}
-
-resource "azurerm_key_vault" "example" {
-  name                = "example-keyvault"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-
+resource "azurerm_key_vault" "key_vault" {
+  name                        = var.key_vault
+  location                    = var.location
+  resource_group_name         = var.resource_group
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+  soft_delete_retention_days  = var.soft_delete_retention_days
+  purge_protection_enabled    = var.purge_protection_enabled
+  enabled_for_disk_encryption = var.enabled_for_disk_encryption
+  tags                        = var.tags
   network_acls {
-    default_action = "Deny"
+    bypass                     = "AzureServices"
+    default_action             = var.default_action
+    ip_rules                   = var.ip_rules
+    virtual_network_subnet_ids = var.subnet_ids
+  }
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
   }
 }
